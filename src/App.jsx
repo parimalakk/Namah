967ac349c222e3892e4ba3adae28f17e306b4482
parimalakk <vakapari@gmail.com>
2025-05-08 23:19:15 +0530

    sonu

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hira from './Components/Hira'
+import Sony from './Components/Sony'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <Hira/>
+   <Sony/>
   )
 }
 
---
+++ src/Components/Sony.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sony() {
+  return (
+    <div>
+      <h1>sonu</h1>
+    </div>
+  )
+}
+
+export default Sony
